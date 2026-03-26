b864d722405e866a5ebdc9d7ecc986c58ebc75d5
Younes Tsouli <younestsouli444@gmail.com>
2026-03-26 22:41:33 +0100

    feat: added the home page and now working on the housing location componant and the rendering of the values using Input and the for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  imports: [HeaderComponent, HomeComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes";
 }
