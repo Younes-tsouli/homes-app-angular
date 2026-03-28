6fc068c834649b2490455989ca4c8731c8ad50ec
Younes Tsouli <younestsouli444@gmail.com>
2026-03-28 16:45:07 +0100

    feat: adding a service + form

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,18 +1,40 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HousingLocation } from "src/app/housingLocation";
+import { HousingService } from "src/app/housing.service";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-details',
+  selector: "app-details",
   standalone: true,
-  imports: [],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  imports: [ReactiveFormsModule],
+  templateUrl: "./details.component.html",
+  styleUrl: "./details.component.css",
 })
 export class DetailsComponent {
-  route:ActivatedRoute = inject(ActivatedRoute)
-  housingLocationId = 0
+  route: ActivatedRoute = inject(ActivatedRoute);
 
-  constructor() { //used to make changes
-      this.housingLocationId = Number(this.route.snapshot.params["id"]) // we get the id of the page
+  housingService: HousingService = inject(HousingService);
+  housingLocation:HousingLocation | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email])
+  })
+
+  constructor() {
+    //used to make changes
+    const id = Number(this.route.snapshot.params["id"]); // we get the id of the page
+    this.housingLocation = this.housingService.getHousingLocationById(id)
+  }
+
+  submiForm() {
+    this.housingService.submitForm(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
+    this.applyForm.reset()
   }
 }
--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,16 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HousingLocation } from 'src/app/housing-location';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "src/app/housingLocation";
 import { RouterLink, RouterModule } from "@angular/router";
 
-
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   standalone: true,
   imports: [CommonModule, RouterLink, RouterModule],
-  templateUrl: './housing-location.component.html',
-  styleUrls: ['./housing-location.component.css']
+  templateUrl: "./housing-location.component.html",
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
-    @Input() housingLocation !: HousingLocation;
+  @Input() housingLocation!: HousingLocation;
 }
